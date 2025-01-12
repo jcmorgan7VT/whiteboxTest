3ea221bb7f1ae1fc06e70d7e4f027b36142dcda9
John Morgan <jcmorgan7@vt.edu>
2025-01-12 17:08:28 -0500

    adding shinylive files

---
+++ app/app.R
@@ -0,0 +1,40 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+library(shiny)
+library(shiny)
+library(whitebox)
+library(terra)
+ui <- fluidPage(
+  
+  titlePanel(paste("WhiteboxTools-Shiny Test", getwd())),
+  
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("threshold", "Threshold:", min = 1, max = 40, value = 8),
+      checkboxInput("compress", "Compress Rasters?")
+    ),
+    mainPanel(
+      plotOutput("slopePlot")
+    )
+  )
+)
+server <- function(input, output) {
+  # setwd("~")
+  # wbt_wd("")
+  output$slopePlot <- renderPlot({
+    # generate threshold slope map based on input$threshold from ui.R
+    x <- wbt_slope(sample_dem_data(destfile = "~/input.tif"), "~/output.tif")
+    # , compress_rasters = input$compress)
+    terra::plot(terra::rast("~/output.tif") > input$threshold)
+    unlink("~/output.tif")
+  })
+}
+shinyApp(ui = ui, server = server)
+##shinylive::export(appdir = "latFlow", destdir = "docs")
